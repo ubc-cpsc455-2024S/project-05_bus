7383c725cc309d2931bb0b2329591765485bc136
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-06-05 14:54:42 -0700

    Add profile popover

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,14 @@
 import './Navbar.css';
-import { IconButton, ButtonGroup, Avatar } from '@chakra-ui/react'
+import { 
+  IconButton,
+  ButtonGroup,
+  Avatar,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+} from '@chakra-ui/react'
 import { Home, CalendarMonth, Restaurant, Person } from '@mui/icons-material'
 
 const Navbar = () => {
@@ -12,12 +21,25 @@ const Navbar = () => {
           <IconButton href="/groceries" icon={<Restaurant sx={{fontSize: 30}}/>} size="lg"/>
         </nav>
       </ButtonGroup>
-      <IconButton 
-        href="/profile"
-        className="profile-button"
-        icon={<Avatar size="sm" bg="none" icon={<Person className="default-user-icon" sx={{fontSize: 30}}/>} />} 
-        size="lg"
-        />
+      <Popover placement="right-start" isLazy>
+        <PopoverTrigger>
+          <IconButton 
+          className="profile-button"
+          icon={<Avatar size="sm" bg="none" icon={<Person className="default-user-icon" sx={{fontSize: 30}}/>} />} 
+          size="lg"
+          />
+        </PopoverTrigger>
+        <PopoverContent className="profile-popover-content">
+          <PopoverHeader className="profile-popover-header">
+            <Avatar size="sm" className="profile-popover-avatar"/>
+            <h2 className="profile-popover-name">John Doe</h2>
+          </PopoverHeader>
+          <PopoverBody className="profile-popover-links">
+            <a href="/profile">Profile</a>
+            <a href="/settings">Settings</a>
+          </PopoverBody>
+        </PopoverContent>
+      </Popover>
     </div>
   )
 }
