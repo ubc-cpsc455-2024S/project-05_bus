5c672ebfa63590a5e8b33b443af9aba27cd9d6e8
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-06-06 17:38:41 -0700

    Add routes

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,8 +1,12 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import { Routes, Route, useLocation } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 import CalendarPage from './pages/CalendarPage'
 import LoginPage from './pages/LoginPage'
+import GroceriesPage from './pages/GroceriesPage'
+import ProfilePage from './pages/ProfilePage'
+import SettingsPage from './pages/SettingsPage'
 
 function Layout() {
   const location = useLocation();
@@ -13,13 +17,13 @@ function Layout() {
       {noNavbarPaths.includes(location.pathname) ? null : <Navbar />}
       <div className={noNavbarPaths.includes(location.pathname) ? "" : "navbar-padding"}>
         <Routes>
-          {/* <Route exact path="/" element={<HomePage />} /> */}
+          <Route exact path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           {/* <Route path="/signup" element={<SignupPage />} /> */}
           <Route path="/calendar" element={<CalendarPage />} />
-          {/* <Route path="/groceries" element={<GroceriesPage />} /> */}
-          {/* <Route path="/profile" element={<ProfilePage />} /> */}
-          {/* <Route path="/settings" element={<SettingsPage />} /> */}
+          <Route path="/groceries" element={<GroceriesPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/settings" element={<SettingsPage />} />
         </Routes>
       </div>
     </>
