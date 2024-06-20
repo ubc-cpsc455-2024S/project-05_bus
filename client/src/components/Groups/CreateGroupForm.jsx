97172560100f7698153ab44800c318f4f75fea42
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-06-19 21:02:20 -0700

    Add group creation logic

--- client/src/components/Groups/CreateGroupForm.jsx
+++ client/src/components/Groups/CreateGroupForm.jsx
@@ -1,10 +1,15 @@
 import './CreateGroupForm.css';
+import { nanoid } from 'nanoid';
 import { Button, Box, Input, Tag, TagLabel, TagCloseButton, VStack, Text, HStack, FormControl, FormLabel } from '@chakra-ui/react';
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { createGroup } from '../../redux/slices/groupsSlice';
+import { updateUser } from '../../redux/slices/usersSlice';
 
 export default function CreateGroupForm() {
-  const users = useSelector((state) => state.users.users);
+  const dispatch = useDispatch();
+  const users = useSelector(state => state.users.users);
+  const currentUser = useSelector(state => state.users.currentUser);
   const [groupName, setGroupName] = useState('');
   const [emailInput, setEmailInput] = useState('');
   const [selectedUsers, setSelectedUsers] = useState([]);
@@ -23,8 +28,12 @@ export default function CreateGroupForm() {
     const user = users.find((user) => user.email === emailInput);
     if (user) {
       if (!selectedUsers.find((u) => u.id === user.id)) {
-        setSelectedUsers([...selectedUsers, user]);
-        setEmailInput('');
+        if (user.groupID) {
+          setError('User already belongs to a group.');
+        } else {
+          setSelectedUsers([...selectedUsers, user]);
+          setEmailInput('');
+        }
       } else {
         setError('User is already added');
       }
@@ -36,6 +45,18 @@ export default function CreateGroupForm() {
   const handleRemoveUser = (userId) => {
     setSelectedUsers(selectedUsers.filter((user) => user.id !== userId));
   };
+
+  const handleCreateGroup = () => {
+    const members = [currentUser, ...selectedUsers];
+    const memberIDs = members.map(member => member.id);
+    const group = {
+      id: nanoid(),
+      name: groupName,
+      memberIDs
+    }
+    dispatch(createGroup(group));
+    dispatch(updateUser({id: currentUser.id, updatedFields: {groupID: group.id}}));
+  }
 
   return (
     <Box width="400px" margin="auto" mt="20px">
@@ -66,13 +87,13 @@ export default function CreateGroupForm() {
       )}
       <VStack align="start" mt="10px">
         {selectedUsers.map((user) => (
-          <Tag key={user.id} size="lg" variant="solid" colorScheme="blue">
+          <Tag key={user.id} size="lg" variant="solid" colorScheme="teal">
             <TagLabel>{user.email}</TagLabel>
             <TagCloseButton onClick={() => handleRemoveUser(user.id)} />
           </Tag>
         ))}
       </VStack>
-      <Button type="submit" mt="20px" isDisabled={!groupName}>
+      <Button mt="20px" isDisabled={!groupName} onClick={handleCreateGroup}>
         Create Group
       </Button>
     </Box>
