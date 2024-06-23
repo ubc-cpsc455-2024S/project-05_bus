97b442bb958aac008bf3b6caf864e4d2ea88397a
Jwong105 <jennifer.wong105@gmail.com>
2024-06-22 20:55:29 -0700

    Connect mongoose

--- server/app.js
+++ server/app.js
@@ -2,19 +2,38 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+
+require('dotenv').config()
+const mongoose = require('mongoose');
+const db_uri = process.env.DB_URI;
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Connect Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-module.exports = app;
+// Connect Database
+async function connectDB() {
+    try {
+        await mongoose.connect(db_uri, clientOptions);
+        console.log('Successfully connected to MongoDB.');
+    } catch (err) {
+        console.error('MongoDB connection error:', err);
+        throw err;
+    }
+};
+
+module.exports = { app, connectDB };
