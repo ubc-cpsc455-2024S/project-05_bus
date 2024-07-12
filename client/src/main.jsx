a50bb67c364881e199d6e137193e9a2c751c4fa3
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-07-12 15:52:23 -0700

    Add persistence

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
