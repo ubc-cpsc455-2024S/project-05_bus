552b6e7e1d1a2e320b5528aa68bc71714a10bbf2
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-07-23 19:28:07 -0700

    Add alerts for leave and delete group

--- client/src/pages/SettingsPage.jsx
+++ client/src/pages/SettingsPage.jsx
@@ -1,5 +1,5 @@
 import './SettingsPage.css';
-import { Box, Card, CardHeader, CardBody, CardFooter, Button, Tooltip, Divider, Text } from '@chakra-ui/react';
+import { Box, Card, CardHeader, CardBody, CardFooter, Button, Tooltip, Divider, Text, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from '@chakra-ui/react';
 import GroupNameSettings from '../components/Groups/Settings/GroupNameSettings';
 import MembersSettings from '../components/Groups/Settings/MembersSettings';
 import AdminsSettings from '../components/Groups/Settings/AdminsSettings';
@@ -10,13 +10,81 @@ import { useNavigate } from 'react-router-dom';
 import { selectIsCurrentUserAdmin } from '../selectors/groupsSelectors';
 import { deleteGroupAsync, removeMemberAsync } from '../redux/groups/thunks';
 
+function LeaveGroupModal({ isOnlyMember, isOnlyAdmin, isOpen, onClose, onConfirm }) {
+  if (!isOpen) return null;
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Confirm Action</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          {isOnlyMember ? (
+            <Text>You are the only member of this group. Leaving the group will delete it. Are you sure you want to leave?</Text>
+          ) : isOnlyAdmin ? (
+            <Text>You are the only admin in this group. Please assign another admin before leaving.</Text>
+          ) : (
+            <Text>Are you sure you want to leave this group?</Text>
+          )}
+        </ModalBody>
+        <ModalFooter>
+          <Button 
+            bgColor={"rgba(253, 163, 163, 0.631)"}
+            _hover={{background: "rgba(246, 134, 134, 0.631)"}}
+            mr={3}
+            onClick={onConfirm}
+          >
+            Confirm
+          </Button>
+          <Button variant="outline" onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+
+function DeleteGroupAlert({ isOpen, onClose, onConfirm }) {
+  if (!isOpen) return null;
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Confirm Deletion</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text>Are you sure you want to delete this group? This action cannot be undone.</Text>
+        </ModalBody>
+        <ModalFooter>
+        <Button 
+            bgColor={"rgba(253, 163, 163, 0.631)"}
+            _hover={{background: "rgba(246, 134, 134, 0.631)"}}
+            mr={3}
+            onClick={onConfirm}
+          >
+            Delete Group
+          </Button>
+          <Button variant="outline" onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
 export default function Settings() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const currentGroup = useCurrentGroup();
   const currentUserID = useSelector((state) => state.users.currentUserID);
   const isAdmin = useSelector((state) => selectIsCurrentUserAdmin(state, currentGroup));
+  const isOnlyMember = currentGroup ? currentGroup.memberIDs.length === 1 : null;
+  const isOnlyAdmin = currentGroup ? isAdmin && currentGroup.adminIDs.length === 1 : null;
   const [error, setError] = useState('');
+
+  const { isOpen: isLeaveGroupOpen, onOpen: openLeaveGroupModal, onClose: closeLeaveGroupModal } = useDisclosure();
+  const { isOpen: isDeleteGroupOpen, onOpen: openDeleteGroupModal, onClose: closeDeleteGroupModal } = useDisclosure();
 
   useEffect(() => {
     if (!currentGroup) {
@@ -25,11 +93,16 @@ export default function Settings() {
   }, [currentGroup, navigate]);
 
   const handleLeaveGroup = async () => {
-    try {
-      await dispatch(removeMemberAsync({groupID: currentGroup._id, userID: currentUserID})).unwrap();
-    } catch (error) {
-      setError('Could not remove user from group');
+    if (isOnlyMember) {
+      return await handleDeleteGroup();
+    } else {
+      try {
+        await dispatch(removeMemberAsync({ groupID: currentGroup._id, userID: currentUserID })).unwrap();
+      } catch (error) {
+        setError('Could not remove user from group');
+      }
     }
+    closeLeaveGroupModal();
   };
 
   const handleDeleteGroup = async () => {
@@ -43,51 +116,73 @@ export default function Settings() {
   return (
     <Box className="settings-container">
       {currentGroup && 
-        <Card className="group-settings-card">
-          <CardHeader>
-            <div className="group-settings-header">
-              <div className="group-settings-heading">
-                <span className="material-symbols-outlined settings-group-icon icon">house</span>
-                <h1 className="group-settings-heading-text">Group Settings</h1>
+        <div>
+          <Card className="group-settings-card">
+            <CardHeader>
+              <div className="group-settings-header">
+                <div className="group-settings-heading">
+                  <span className="material-symbols-outlined settings-group-icon icon">house</span>
+                  <h1 className="group-settings-heading-text">Group Settings</h1>
+                </div>
+                <div className="group-settings-edit-container">
+                  <Tooltip label="Only admins can edit groups." isDisabled={isAdmin}>
+                    <Button className="group-settings-edit-button" isDisabled={!isAdmin}>
+                      <span className="material-symbols-outlined group-settings-edit-icon">edit_square</span>
+                    </Button>
+                  </Tooltip>
+                </div>
               </div>
-              <div className="group-settings-edit-container">
-                <Tooltip label="Only admins can edit groups." isDisabled={isAdmin}>
-                  <Button className="group-settings-edit-button" isDisabled={!isAdmin}>
-                    <span className="material-symbols-outlined group-settings-edit-icon">edit_square</span>
-                  </Button>
-                </Tooltip>
-              </div>
-            </div>
-          </CardHeader>
-          <CardBody className="home-card-body">
-            <GroupNameSettings group={currentGroup}/>
-            <Divider borderColor="rgba(0, 128, 128, 0.631)" borderRadius="5px"/>
-            <MembersSettings />
-            <Divider borderColor="rgba(0, 128, 128, 0.631)" borderRadius="5px"/>
-            <AdminsSettings group={currentGroup}/>
-          </CardBody>
-          <CardFooter className="group-settings-footer">
-            <Button className="group-settings-leave-button" onClick={handleLeaveGroup}>
-              Leave Group
-            </Button>
-            <Tooltip label="Only admins can delete groups." isDisabled={isAdmin}>
-              <Button 
-                className="group-settings-delete-button" 
-                isDisabled={!isAdmin} 
-                bgColor={"rgba(253, 163, 163, 0.631)"} 
-                _hover={{background: "rgba(246, 134, 134, 0.631)"}}
-                onClick={handleDeleteGroup}
-              >
-                Delete Group
+            </CardHeader>
+            <CardBody className="home-card-body">
+              <GroupNameSettings group={currentGroup}/>
+              <Divider borderColor="rgba(0, 128, 128, 0.631)" borderRadius="5px"/>
+              <MembersSettings />
+              <Divider borderColor="rgba(0, 128, 128, 0.631)" borderRadius="5px"/>
+              <AdminsSettings group={currentGroup}/>
+            </CardBody>
+            <CardFooter className="group-settings-footer">
+              <Button className="group-settings-leave-button" onClick={openLeaveGroupModal}>
+                Leave Group
               </Button>
-            </Tooltip>
-            {error && (
-              <Text color="red.500" mb="10px">
-                {error}
-              </Text>
-            )}
-          </CardFooter>
-        </Card>
+              <Tooltip label="Only admins can delete groups." isDisabled={isAdmin}>
+                <Button 
+                  className="group-settings-delete-button" 
+                  isDisabled={!isAdmin} 
+                  bgColor={"rgba(253, 163, 163, 0.631)"} 
+                  _hover={{background: "rgba(246, 134, 134, 0.631)"}}
+                  onClick={openDeleteGroupModal}
+                >
+                  Delete Group
+                </Button>
+              </Tooltip>
+              {error && (
+                <Text color="red.500" mb="10px">
+                  {error}
+                </Text>
+              )}
+            </CardFooter>
+          </Card>
+
+          <LeaveGroupModal 
+          isOpen={isLeaveGroupOpen} 
+          onClose={closeLeaveGroupModal}
+          onConfirm={async () => {
+            closeLeaveGroupModal();
+            await handleLeaveGroup();
+          }}
+          isOnlyMember={isOnlyMember}
+          isOnlyAdmin={isOnlyAdmin}
+        />
+
+        <DeleteGroupAlert 
+          isOpen={isDeleteGroupOpen} 
+          onClose={closeDeleteGroupModal} 
+          onConfirm={async () => {
+            closeDeleteGroupModal();
+            await handleDeleteGroup();
+          }}
+        />
+      </div>
       }
     </Box>
   )
