1c852644bf0cfa7694474f2f2564b5f04d5b68a5
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-07-23 18:28:36 -0700

    Add leave and delete group functionality

--- client/src/pages/SettingsPage.jsx
+++ client/src/pages/SettingsPage.jsx
@@ -1,66 +1,94 @@
 import './SettingsPage.css';
-import { Box, Card, CardHeader, CardBody, CardFooter, Button, Tooltip, Divider } from '@chakra-ui/react';
+import { Box, Card, CardHeader, CardBody, CardFooter, Button, Tooltip, Divider, Text } from '@chakra-ui/react';
 import GroupNameSettings from '../components/Groups/Settings/GroupNameSettings';
 import MembersSettings from '../components/Groups/Settings/MembersSettings';
 import AdminsSettings from '../components/Groups/Settings/AdminsSettings';
 import useCurrentGroup from '../hooks/useCurrentGroup';
-import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { selectIsCurrentUserAdmin } from '../selectors/groupsSelectors';
+import { deleteGroupAsync, removeMemberAsync } from '../redux/groups/thunks';
 
 export default function Settings() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const currentGroup = useCurrentGroup();
+  const currentUserID = useSelector((state) => state.users.currentUserID);
   const isAdmin = useSelector((state) => selectIsCurrentUserAdmin(state, currentGroup));
+  const [error, setError] = useState('');
 
-  const handleLeaveGroup = () => {
+  useEffect(() => {
+    if (!currentGroup) {
+      navigate('/groups');
+    }
+  }, [currentGroup, navigate]);
 
+  const handleLeaveGroup = async () => {
+    try {
+      await dispatch(removeMemberAsync({groupID: currentGroup._id, userID: currentUserID})).unwrap();
+    } catch (error) {
+      setError('Could not remove user from group');
+    }
   };
 
-  const handleDeleteGroup = () => {
-
+  const handleDeleteGroup = async () => {
+    try {
+      await dispatch(deleteGroupAsync({groupID: currentGroup._id, currentUserID})).unwrap();
+    } catch (error) {
+      setError('Could not delete group');
+    }
   };
 
   return (
     <Box className="settings-container">
-      <Card className="group-settings-card">
-        <CardHeader>
-          <div className="group-settings-header">
-            <div className="group-settings-heading">
-              <span className="material-symbols-outlined settings-group-icon icon">house</span>
-              <h1 className="group-settings-heading-text">Group Settings</h1>
+      {currentGroup && 
+        <Card className="group-settings-card">
+          <CardHeader>
+            <div className="group-settings-header">
+              <div className="group-settings-heading">
+                <span className="material-symbols-outlined settings-group-icon icon">house</span>
+                <h1 className="group-settings-heading-text">Group Settings</h1>
+              </div>
+              <div className="group-settings-edit-container">
+                <Tooltip label="Only admins can edit groups." isDisabled={isAdmin}>
+                  <Button className="group-settings-edit-button" isDisabled={!isAdmin}>
+                    <span className="material-symbols-outlined group-settings-edit-icon">edit_square</span>
+                  </Button>
+                </Tooltip>
+              </div>
             </div>
-            <div className="group-settings-edit-container">
-              <Tooltip label="Only admins can edit groups." isDisabled={isAdmin}>
-                <Button className="group-settings-edit-button" isDisabled={!isAdmin}>
-                  <span className="material-symbols-outlined group-settings-edit-icon">edit_square</span>
-                </Button>
-              </Tooltip>
-            </div>
-          </div>
-        </CardHeader>
-        <CardBody className="home-card-body">
-          <GroupNameSettings group={currentGroup}/>
-          <Divider borderColor="rgba(0, 128, 128, 0.631)" borderRadius="5px"/>
-          <MembersSettings />
-          <Divider borderColor="rgba(0, 128, 128, 0.631)" borderRadius="5px"/>
-          <AdminsSettings group={currentGroup}/>
-        </CardBody>
-        <CardFooter className="group-settings-footer">
-          <Button className="group-settings-leave-button" onClick={handleLeaveGroup}>
-            Leave Group
-          </Button>
-          <Tooltip label="Only admins can delete groups." isDisabled={isAdmin}>
-            <Button 
-              className="group-settings-delete-button" 
-              isDisabled={!isAdmin} 
-              bgColor={"rgba(253, 163, 163, 0.631)"} 
-              _hover={{background: "rgba(246, 134, 134, 0.631)"}}
-              onClick={handleDeleteGroup}
-            >
-              Delete Group
+          </CardHeader>
+          <CardBody className="home-card-body">
+            <GroupNameSettings group={currentGroup}/>
+            <Divider borderColor="rgba(0, 128, 128, 0.631)" borderRadius="5px"/>
+            <MembersSettings />
+            <Divider borderColor="rgba(0, 128, 128, 0.631)" borderRadius="5px"/>
+            <AdminsSettings group={currentGroup}/>
+          </CardBody>
+          <CardFooter className="group-settings-footer">
+            <Button className="group-settings-leave-button" onClick={handleLeaveGroup}>
+              Leave Group
             </Button>
-          </Tooltip>
-        </CardFooter>
-      </Card>
+            <Tooltip label="Only admins can delete groups." isDisabled={isAdmin}>
+              <Button 
+                className="group-settings-delete-button" 
+                isDisabled={!isAdmin} 
+                bgColor={"rgba(253, 163, 163, 0.631)"} 
+                _hover={{background: "rgba(246, 134, 134, 0.631)"}}
+                onClick={handleDeleteGroup}
+              >
+                Delete Group
+              </Button>
+            </Tooltip>
+            {error && (
+              <Text color="red.500" mb="10px">
+                {error}
+              </Text>
+            )}
+          </CardFooter>
+        </Card>
+      }
     </Box>
   )
 }
--- client/src/redux/groups/thunks.js
+++ client/src/redux/groups/thunks.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { actionTypes } from "./actionTypes";
 import GroupsService from "./service";
+import { setUserGroupID } from "../users/usersSlice";
 
 export const getGroupsAsync = createAsyncThunk(
   actionTypes.GET_GROUPS,
@@ -29,15 +30,27 @@ export const updateGroupNameAsync = createAsyncThunk(
 
 export const addMemberAsync = createAsyncThunk(
   actionTypes.ADD_MEMBER,
-  async ({groupID, userID}) => {
-    return await GroupsService.addGroupMember(groupID, userID);
+  async ({ groupID, userID }, thunkAPI) => {
+    try {
+      const updatedGroup = await GroupsService.addGroupMember(groupID, userID);
+      thunkAPI.dispatch(setUserGroupID({ userID, groupID }));
+      return updatedGroup;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 
 export const removeMemberAsync = createAsyncThunk(
   actionTypes.REMOVE_MEMBER,
-  async ({groupID, userID}) => {
-    return await GroupsService.removeGroupMember(groupID, userID);
+  async ({ groupID, userID }, thunkAPI) => {
+    try {
+      const updatedGroup = await GroupsService.removeGroupMember(groupID, userID);
+      thunkAPI.dispatch(setUserGroupID({ userID, groupID: null }));
+      return updatedGroup;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
 
@@ -57,8 +70,13 @@ export const removeAdminAsync = createAsyncThunk(
 
 export const deleteGroupAsync = createAsyncThunk(
   actionTypes.DELETE_GROUP,
-  async (id) => {
-    const response = await GroupsService.deleteGroup(id);
-    return response;
+  async ({groupID, currentUserID}, thunkAPI) => {
+    try {
+      const deletedGroup = await GroupsService.deleteGroup(groupID);
+      thunkAPI.dispatch(setUserGroupID({ userID: currentUserID, groupID: null }));
+      return deletedGroup;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
 );
