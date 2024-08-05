19231fed9365afc91af3e518d4dc403b0b37e111
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-08-05 13:33:07 -0700

    Fix persistence issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,7 @@
 import './App.css';
 import Layout from './Layout';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { getUsersAsync } from './redux/users/thunks';
-import { getGroupsAsync } from './redux/groups/thunks';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getGroupsAsync());
-    dispatch(getUsersAsync());
-  }, [dispatch]);
-
   return (
     <Layout />
   );
