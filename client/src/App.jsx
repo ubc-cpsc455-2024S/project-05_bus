6c921e9d3bcba5e9bce666bca5e958b9fe0677df
jocelynbachmann <82013530+jocelynbachmann@users.noreply.github.com>
2024-08-05 14:23:20 -0700

    Add group protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from 'react';
 import './App.css';
 import Layout from './Layout';
+import { useDispatch } from 'react-redux';
+import { getUsersAsync } from './redux/users/thunks';
+import { getGroupsAsync } from './redux/groups/thunks';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGroupsAsync());
+    dispatch(getUsersAsync());
+  }, [dispatch]);
+
   return (
     <Layout />
   );
